d3378a21eec4f7cdf38be1ac1d3f16c207bc5838
vitorbclemes <vitorbc.oficial@gmail.com>
2022-06-08 14:02:40 -0300

    initial dataStorageImplementation

--- utils/DataStorage.js
+++ utils/DataStorage.js
@@ -1,15 +1,29 @@
-async function get(table,obj){
-  let response = await fetch(`/${table}?${new URLSearchParams(obj)}`);
-  return response.ok? await response.json() : null;
-}
-
-async function post(table,obj){
-  let response = await fetch (`/${table}`,{
-    method:'post',
-    body:JSON.stringify(obj),
-    headers:{
-      'content-type':'application/json'
-    }
-  });
-  return response.ok? await response.json() : null;
+export default {
+  async get(table,obj){
+    let response = await fetch(`/${table}?${new URLSearchParams(obj)}`);
+    return response.ok? await response.json() : null;
+  },
+  
+  async post(table,obj){
+    let response = await fetch (`/${table}`,{
+      method:'post',
+      body:JSON.stringify(obj),
+      headers:{
+        'content-type':'application/json'
+      }
+    });
+    return response.ok? await response.json() : null;
+  },
+  
+  async put(table,obj){
+    let response = await fetch(`/${table}`,{
+      method:'put',
+      body:JSON.stringify(obj),
+      headers:{
+        'content-type':'application/json'
+      }
+    });
+    return response.ok? await response.json() : null;
+  }
 };
+
